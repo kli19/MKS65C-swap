7cafc2ccb196f6ad7f2e557ca06334a07046a662
Karen Li <kli19@stuy.edu>
2018-09-19 16:47:53 -0400

    tested seed

--- swap.c
+++ swap.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
 
 int main(){
 
+  time_t currentTime = time(NULL);
+  int arr[10];
+
+  printf("This is the current time: %s", ctime(&currentTime));
+  srand(currentTime);
+  
+  
 }
