0a378798c77a78df53e892658c00fbc21d364563
Karen Li <kli19@stuy.edu>
2018-09-19 17:39:29 -0400

    populated 2nd array with values in first array in reverse order

--- swap.c
+++ swap.c
@@ -12,20 +12,30 @@ int main(){
 
   int arr[10];
   int ARR[10];
+  int *arr_Pointer = arr;
+  int *ARR_Pointer = ARR + 9;
 
+  //populating first 9 slots of arr with random ints
   for (int i = 0; i < 9; i++){
     arr[i] = rand();
   }
 
+  //setting last slot of arr to 0
   arr[9] = rand();
 
-  
+  for (int i = 0; i < 10; i++){
+    *(ARR_Pointer - i)   = *(arr_Pointer + i);
+  }
+
+  printf("-----------------------PRINTING OUT FIRST ARRAY-----------------------\n"); 
   for (int i = 0; i < 10; i++){
     printf("arr[%d]: %d\n", i, arr[i]); 
   }
-  
-  
-  
+
+  printf("-----------------------PRINTING OUT SECOND ARRAY-----------------------\n"); 
+  for (int i = 0; i < 10; i++){
+    printf("ARR[%d]: %d\n", i, ARR[i]); 
+  }
   
   
   
